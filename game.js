7d79057721eb58d85198cc3e61a285f4d5fd08cb
Andre <ajparker000@gmail.com>
2020-04-30 18:55:48 -0700

    add animation for correct and incorrect answers

--- game.js
+++ game.js
@@ -43,7 +43,6 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
-  console.log(availableQuestions);
   getNewQuestion();
 };
 
@@ -74,8 +73,17 @@ choices.forEach((choice) => {
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset['number'];
-    console.log(selectedAnswer);
-    getNewQuestion();
+
+    let classToApply = 'incorrect';
+    if (selectedAnswer == currentQuestion.answer) {
+      classToApply = 'correct';
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
 
